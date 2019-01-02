4d5d1efc95b768e9cb9c2611a16ed67c72632328
Monkeylord <bluegod_001@hotmail.com>
2019-01-02 22:25:46 +0800

    Simple Shop Example

---
+++ Examples/SimpleShop.js
@@ -0,0 +1,100 @@
+var Transaction=require('bsv').Transaction;
+var Address=require('bsv').Address;
+var shopConfig=null;
+var isVaild=function(tx){return true};
+var getCommodity=null;
+
+var getPublicKeyInPublicKeyHashIn=function(input){
+    if(input.script.isPublicKeyHashIn()==false)throw new Error("Not PublicKeyHashIn")
+    var script=input.script.toBuffer();
+    var pubkey=script.subarray(script.length-33,script.length);
+    return pubkey;
+}
+
+var getMsg=function(tx){
+    var outputs=Transaction(tx).outputs;
+    for (var i = 0; i < outputs.length; i) {  
+        if(outputs[i].script.isDataOut())return outputs[i].script.toBuffer().subarray(2)
+    }
+    return new Buffer();
+}
+
+var getFirstPayment=function(tx,address){
+    var outputs=Transaction(tx).outputs;
+    for (var i = 0; i < outputs.length; i++) {  
+        if(outputs[i].script.toAddress().toString()==new Address(address).toString())return i;
+    }
+    return null;
+}
+
+var getFirstPKHInput=function(tx){
+    var inputs=Transaction(tx).inputs;
+    for (var i = 0; i < inputs.length; i) {  
+        if(inputs[i].script.isPublicKeyHashIn())return inputs[i];
+    }
+    return null;
+}
+
+var buildRefundTX = function (tx,buyerPK){
+    var paymentIndex = getFirstPayment(tx,shopConfig.ShopAddress)
+    var utxo = {
+        "txId" : tx.hash,
+        "outputIndex" : paymentIndex,
+        "address" : shopConfig.ShopAddress,
+        "script" : tx.outputs[paymentIndex].script,
+        "satoshis" : tx.outputs[paymentIndex].satoshis
+    }
+    var refundTX = new Transaction()
+                    .from(utxo).addData(ECIES.encrypt('Refund',buyerPK))
+                    .change(new PublicKey(buyerPK).toAddress())
+                    .feePerKb(1024)
+    return refundTX
+}
+
+var buildDeliverTX = function (tx,buyerPK,commodity){
+    var paymentIndex = getFirstPayment(tx,shopConfig.ShopAddress)
+    var utxo = {
+        "txId" : tx.hash,
+        "outputIndex" : paymentIndex,
+        "address" : shopConfig.ShopAddress,
+        "script" : tx.outputs[paymentIndex].script,
+        "satoshis" : tx.outputs[paymentIndex].satoshis
+    }
+    var deliverTX = new Transaction()
+                        .from(utxo).addData(ECIES.encrypt(commodity,buyerPK))
+                        .to(new PublicKey(buyerPK).toAddress(),1)
+                        .change(shopConfig.StorageAddress)
+                        .feePerKb(1024)
+    return deliverTX
+}
+
+var handlePayment = function(tx){
+    if(shopConfig==null)throw new Error("shop config first");
+    //Get Buyer PublicKey from Payment TX Input
+    var firstPKH=getFirstPKHInput(tx)
+    var buyerPK=getPublicKeyInPublicKeyHashIn(firstPKH)
+    if(isVaild(tx)){
+        //Get OP_RETURN content
+        var msg=getMsg(tx)
+        //Get Commodity according to msg
+        var commodity=getCommodity(msg)
+        //Build DeliverTX
+        return buildDeliverTX(tx,buyerPK,commodity)
+    }else{
+        return buildRefundTX(tx,buyerPK)
+    }
+}
+
+var config = function(config,commodityProvider,validator){
+    shopConfig=config;
+    getCommodity=commodityProvider;
+    isVaild=validator;
+}
+
+module.exports.getPublicKeyInPublicKeyHashIn=getPublicKeyInPublicKeyHashIn;
+module.exports.getFirstPayment=getFirstPayment
+module.exports.getFirstPKHInput=getFirstPKHInput;
+module.exports.buildRefundTX=buildRefundTX;
+module.exports.buildDeliverTX=buildDeliverTX;
+module.exports.handlePayment=handlePayment;
+module.exports.config=config;
---
+++ Examples/SimpleShopExample.js
@@ -0,0 +1,43 @@
+const bsv = require('bsv')
+const SimpleShop=require('./SimpleShop.js')
+const ECIES = require('electrum-ecies')
+
+const Transaction = bsv.Transaction
+const PublicKey = bsv.PublicKey
+
+
+//Fetch TX from somewhere. Electrum Node or BitDB or other RPC point
+//It should be a payment to shop address
+
+//var testPaymentTX=new bsv.Transaction('01000000013216d667c54d6d3f95b054084a17bacccb7cdcc43f1caf838b8a40183b62e2eb000000006a473044022020d11e4db8bf59d221e97d167d9fbf845d9f3ed094b33c9c7e458b06341ed23e02203b91f540d794e53856fa62d0f7b097bee47350db1cd73d14e4341d7b5e6b3c2b41210374d0fca0d78a13b8eb0edff0b2c434a7bd8ee96c9efc767489f3577928e67ad8feffffff0300000000000000000f6a0d54686973206973206120747279a0860100000000001976a91470d57479627e3b77847648a873dff945bfdeb95488ac263a9500000000001976a91461f61eb7233157bad8111b96066d9e0fed87029788ac47870800')
+
+//Config The Shop
+var shopConfig = {
+    'ShopAddress':'1BHcPbcjRZ9ZJvAtr9nd4EQ4HbsUC77WDf', //Shop Address which buyer should pay to
+    'StorageAddress':'19VxNbLGfbXV6UENGSkWmJTQeNLWqqJGaz',  //Cold Wallet Address
+    'FeePerKb':1024
+}
+
+//Define Commodity Provider
+var getCommodity = function (order){
+    //Write your code
+    return "Your Commodity:"+order
+}
+
+//Define Payment Validator
+var isVaild = function(tx){
+    //You Should Validate Payment Amount / Non-P2PKH TX
+    //If return false, A refund TX will be build instead of Deliver TX
+    return true
+}
+
+//Do the Config
+SimpleShop.config(shopConfig,getCommodity,isVaild)
+
+//Then Handle The TX
+var ReplyTX = SimpleShop.handlePayment(testPaymentTX)
+console.log(ReplyTX)
+
+//Then Broadcast the ReplyTX, Electrum Node or BitDB or other RPC point
+var SignedTX = ReplyTX.sign([Your Shop Address Private Key])
+console.log(SignedTX)
